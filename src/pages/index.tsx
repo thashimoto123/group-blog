a49c5d8b3a0b98d13726b08890149d103d16a968
thashimoto <thahsimoto@.me>
2021-05-29 20:46:26 +0900

    fix typescript config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello World!!!<br/>
+          Hello Next & Vercel!!!<br/>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
